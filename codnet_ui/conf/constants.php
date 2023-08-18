b5dde5572040b098affcbcf3de2d36bf543235d6
mpinia76 <marcos.pinero1976@gmail.com>
2023-08-18 17:29:20 -0300

    varios

--- codnet_ui/conf/constants.php
+++ codnet_ui/conf/constants.php
@@ -4,6 +4,6 @@ define('CDT_UI_IMG_SEARCH', WEB_PATH . '/css/cdtui/images/search.gif');
 define('CDT_UI_IMG_EDIT', WEB_PATH . '/css/cdtui/images/edit.gif');
 define('CDT_UI_IMG_DELETE', WEB_PATH . '/css/cdtui/images/del.gif');
 
-define('CDT_UI_UTF8_ENCODE', 1);
+//define('CDT_UI_UTF8_ENCODE', 1);
 
 ?>
